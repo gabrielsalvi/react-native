ef6810613b19657443c1836a0ca733bb18179353
Gabriel Z. Salvi <gabriel.z.salvi@gmail.com>
2022-06-14 14:51:39 -0300

    feat: replace the initial display value with the new value

---
+++ calculator/src/components/Calculator/index.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react'
+import { View } from 'react-native'
+import Button from '../Button'
+import Display from '../Display'
+import styles from './styles'
+
+const initialState = {
+    display: '0',
+}
+
+export default class Calculator extends Component {
+    
+    constructor(props) {
+        super(props)
+        
+        this.state = { ...initialState }
+    }
+
+    changeState = (newState) => this.setState(newState)
+
+    displayText = (newSymbol) => {
+        const display = this.state.display
+
+        this.changeState({
+            display: display === '0' ? newSymbol : display + newSymbol
+        })
+    }
+
+    pressedButton = (symbol) => this.displayText(symbol)
+
+    clearDisplay = () => this.changeState(initialState)
+
+    render() {
+        return (
+            <>
+                <Display toDisplay={this.state.display}/>
+                <View style={styles.buttonsContainer}>
+                    <Button label="AC" onPress={this.clearDisplay} clear />
+                    <Button label="( )" onPress={this.pressedButton} operator />
+                    <Button label="%" onPress={this.pressedButton} operator />
+                    <Button label="/" onPress={this.pressedButton} operator />
+                    <Button label="7" onPress={this.pressedButton} />
+                    <Button label="8" onPress={this.pressedButton} />
+                    <Button label="9" onPress={this.pressedButton} />
+                    <Button label="x" onPress={this.pressedButton} operator />
+                    <Button label="4" onPress={this.pressedButton} />
+                    <Button label="5" onPress={this.pressedButton} />
+                    <Button label="6" onPress={this.pressedButton} />
+                    <Button label="-" onPress={this.pressedButton} operator />
+                    <Button label="1" onPress={this.pressedButton} />
+                    <Button label="2" onPress={this.pressedButton} />
+                    <Button label="3" onPress={this.pressedButton} />
+                    <Button label="+" onPress={this.pressedButton} operator />
+                    <Button label="0" onPress={this.pressedButton} />
+                    <Button label="." onPress={this.pressedButton} />
+                    <Button label="<x" onPress={this.pressedButton} />
+                    <Button label="=" onPress={this.pressedButton} operator />
+                </View>
+            </>
+        )
+    }
+}
---
+++ calculator/src/components/Calculator/styles.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+    buttonsContainer: {
+        flex: 3,
+        flexDirection: "row",
+        flexWrap: "wrap",
+        alignContent: "center",
+        backgroundColor: "#1D1C1C",
+        padding: 5
+    }
+})
+
+export default styles
