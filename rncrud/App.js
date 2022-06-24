01eb4953ea5959636eddd3b068532777a0a7217a
Gabriel Z. Salvi <gabriel.z.salvi@gmail.com>
2022-06-24 13:26:04 -0300

    customize the header of the app

--- rncrud/App.js
+++ rncrud/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Button, SafeAreaView } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import UserForm from './src/views/UserForm';
@@ -7,10 +8,36 @@ import UserList from './src/views/UserList';
 const Stack = createNativeStackNavigator()
 
 export default () => (
-    <NavigationContainer>
-        <Stack.Navigator initialRouteName='UserList'>
-            <Stack.Screen name="UserList" component={UserForm} />
-            <Stack.Screen name="UserForm" component={UserList} />
-        </Stack.Navigator>
-    </NavigationContainer>
+    <SafeAreaView style={{ flex : 1 }}>
+        <NavigationContainer>
+            <Stack.Navigator 
+                initialRouteName='UserList' 
+                screenOptions={{
+                    headerStyle: {
+                        backgroundColor: '#000',
+                    },
+                    headerTitleStyle: {
+                        fontSize: 20,
+                    },
+                    headerTintColor: '#46E74B',
+                    headerTitleAlign: 'left',
+                }}
+            >
+                <Stack.Screen name="UserList" component={UserForm} options={({ navigation }) => {
+                        return {
+                            title: 'List of Users',
+                            headerRight: () => (
+                                <Button
+                                    onPress={() => navigation.navigate("UserForm")}
+                                    type="clear"
+                                    title="New User"
+                                />
+                            )
+                        }
+                    }} 
+                />
+                <Stack.Screen name="UserForm" component={UserList} options={{title: 'Registration Form'}} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    </SafeAreaView>
 )
