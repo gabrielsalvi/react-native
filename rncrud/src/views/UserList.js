a00f5e72f0fa0b8c69f834e2de6df807a34158d1
Gabriel Z. Salvi <gabriel.z.salvi@gmail.com>
2022-06-28 14:45:11 -0300

    feat: add icons to edit and delete an user

--- rncrud/src/views/UserList.js
+++ rncrud/src/views/UserList.js
@@ -1,19 +1,60 @@
 import React, { Component } from 'react';
+import { Alert } from 'react-native';
 import { FlatList } from 'react-native-gesture-handler';
 import { Avatar, ListItem } from "@rneui/themed";
+import AntDesignIcon from 'react-native-vector-icons/AntDesign';
+import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome'
 import users from '../data/users';
 
 export default class App extends Component {    
     
+    confirmUserDeletion = (user) => {
+        Alert.alert('Delete User', `Do you want to delete ${user.name}?`, [
+            {
+                text: 'Yes',
+                onPress: () => console.log('deleted')
+            },
+            {
+                text: 'Nope'
+            }
+        ])
+    }
+
+    confirmUserEdit = (user) => {
+        Alert.alert('Edit User', `Do you want to change the data of ${user.name}?`, [
+            {
+                text: 'Yes',
+                onPress: () =>  this.navigate("UserForm", user)
+            },
+            {
+                text: 'Nope'
+            }
+        ])
+    }
+
+    navigate = (location, param) => this.props.navigation.navigate(location, param)
+
     keyExtractor = (item, index) => index.toString()
 
     renderItem = ({ item: user }) => (
-      <ListItem bottomDivider>
+      <ListItem onPress={() => this.navigate('UserForm', user)} bottomDivider>
         <Avatar source={{uri: user.avatarUrl}} />
         <ListItem.Content>
           <ListItem.Title>{user.name}</ListItem.Title>
           <ListItem.Subtitle>{user.occupation}</ListItem.Subtitle>
         </ListItem.Content>
+        <FontAwesomeIcon
+            name="edit"
+            color="orange"
+            size={25}
+            onPress={() => this.confirmUserEdit(user)}
+        />
+        <AntDesignIcon
+            name="delete"
+            color="red"
+            size={25}
+            onPress={() => this.confirmUserDeletion(user)}
+        />
       </ListItem>
     )
     
