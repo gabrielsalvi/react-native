7b457e1e02cc2daf8471415635abb177931fe353
Gabriel Z. Salvi <gabriel.z.salvi@gmail.com>
2022-06-28 10:46:25 -0300

    downgrade react native version from 0.69 to 0.68 due inconpatibility with library react-native-vector-icons

--- rncrud/android/app/src/main/java/com/crud/newarchitecture/components/MainComponentsRegistry.java
+++ rncrud/android/app/src/main/java/com/crud/newarchitecture/components/MainComponentsRegistry.java
@@ -1,4 +1,4 @@
-package com.rncrud.newarchitecture.components;
+package com.crud.newarchitecture.components;
 
 import com.facebook.jni.HybridData;
 import com.facebook.proguard.annotations.DoNotStrip;
--- rncrud/android/app/src/main/jni/MainComponentsRegistry.h
+++ rncrud/android/app/src/main/jni/MainComponentsRegistry.h
@@ -13,7 +13,7 @@ class MainComponentsRegistry
  public:
   // Adapt it to the package you used for your Java class.
   constexpr static auto kJavaDescriptor =
-      "Lcom/rncrud/newarchitecture/components/MainComponentsRegistry;";
+      "Lcom/crud/newarchitecture/components/MainComponentsRegistry;";
 
   static void registerNatives();
 
