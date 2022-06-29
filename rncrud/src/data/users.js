2fdd2ec9e1876998c79e4278a43595f3dace0ef7
Gabriel Z. Salvi <gabriel.z.salvi@gmail.com>
2022-06-29 19:58:31 -0300

    feat: add id property to the users

--- rncrud/src/data/users.js
+++ rncrud/src/data/users.js
@@ -1,20 +1,24 @@
 export default [
     {
+        id: 1,
         name: 'Amy Farha',
         occupation: 'Web Developer',
         avatarUrl: 'https://cdn.pixabay.com/photo/2016/11/18/23/38/child-1837375_960_720.png',
     },
     {
+        id: 2,
         name: 'Carlos De Peña',
         occupation: 'Athelete',
         avatarUrl: 'https://cdn.pixabay.com/photo/2016/11/01/21/11/avatar-1789663_960_720.png',
     },
     {
+        id: 3,
         name: 'Elon Keller',
         occupation: 'Cook',
         avatarUrl: 'https://cdn.pixabay.com/photo/2016/04/01/12/11/avatar-1300582_960_720.png',
     },
     {
+        id: 4,
         name: 'Mathew Louis',
         occupation: 'Farmer',
         avatarUrl: 'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_960_720.png',
