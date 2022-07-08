7d806acefd10f109b699b3ed858f5df33f6c9b08
Gabriel Z. Salvi <gabriel.z.salvi@gmail.com>
2022-07-08 13:06:38 -0300

    feat: add authentication middleware passportjs

--- tasks/backend/index.js
+++ tasks/backend/index.js
@@ -6,6 +6,7 @@ const db = require('./config/db.js');
 const consign = require('consign');
 
 consign()
+    .include('./config/passport.js')
     .then('./config/middlewares.js')
     .then('./api')
     .then('./config/routes.js')
