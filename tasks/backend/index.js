f332d4fd5367a7de8635431b2e3f47b9f8f529dc
Gabriel Z. Salvi <gabriel.z.salvi@gmail.com>
2022-07-08 11:04:07 -0300

    feat: create route to insert users into the database

--- tasks/backend/index.js
+++ tasks/backend/index.js
@@ -5,13 +5,11 @@ const consign = require('consign');
 
 consign()
     .then('./config/middlewares.js')
+    .then('./api')
+    .then('./config/routes.js')
     .into(app)
 
 app.db = db
-
-app.get('', (req, res) => {
-    res.status(200).send('hello world!')
-})
 
 app.listen(3000, () => {
     console.log('starting')
