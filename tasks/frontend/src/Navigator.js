d33a704013ac9ffbc3a444aa641ce67f8519e9f5
Gabriel Z. Salvi <gabriel.z.salvi@gmail.com>
2022-07-12 09:48:58 -0300

    feat: add drawer navigator

--- tasks/frontend/src/Navigator.js
+++ tasks/frontend/src/Navigator.js
@@ -1,17 +1,37 @@
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { createDrawerNavigator } from '@react-navigation/drawer'
 
 import Auth from './screens/Auth'
 import TaskList from './screens/TaskList'
 
-const Stack = createNativeStackNavigator()
+const Drawer = createDrawerNavigator()
+const DrawerNavigator = () => {
+    return (
+        <Drawer.Navigator initialRouteName='Today' screenOptions={{ headerShown: false}}>
+            <Drawer.Screen name='Today'>
+                {props => <TaskList title='Today' daysAhead={0} {...props} />} 
+            </Drawer.Screen>
+            <Drawer.Screen name='Tomorrow'>
+                {props => <TaskList title='Tomorrow' daysAhead={1} {...props} />} 
+            </Drawer.Screen>
+            <Drawer.Screen name='Week'>
+                {props => <TaskList title='Week' daysAhead={7} {...props} />} 
+            </Drawer.Screen>
+            <Drawer.Screen name='Month'>
+                {props => <TaskList title='Month' daysAhead={30} {...props} />} 
+            </Drawer.Screen>
+        </Drawer.Navigator>
+    )
+}
 
+const Stack = createNativeStackNavigator()
 const StackNavigator = () => {
     return (
         <Stack.Navigator initialRouteName='Auth' screenOptions={{ headerShown: false }}>
             <Stack.Screen name='Auth' component={Auth} />
-            <Stack.Screen name='TaskList' component={TaskList} />
+            <Stack.Screen name='TaskList' component={DrawerNavigator} />
         </Stack.Navigator>
     )
 }
