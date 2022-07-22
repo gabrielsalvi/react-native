dfad668064e66635b38a5d4e2caf9eaead46ce86
Gabriel Z. Salvi <gabriel.z.salvi@gmail.com>
2022-07-22 10:18:00 -0300

    feat: create Contact component

--- contacts/public/styles/colors.js
+++ contacts/public/styles/colors.js
@@ -2,6 +2,7 @@ const colors = {
     main: '#430B68',
     secondary: '#fff',
     text: '#595550',
+    border: '#756580'
 }
 
 export default colors
---
+++ contacts/src/components/Contact.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Gravatar } from 'react-native-gravatar';
+
+import { colors, fonts } from '../../public/styles';
+
+export default props => {
+    const gravatarOptions = {
+        email: props.email,
+        secure: true
+    }
+
+    return (
+        <View style={styles.container}>
+            <Gravatar options={gravatarOptions} style={styles.avatar} />
+            <Text style={styles.name}>{props.name}</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'row',
+        borderBottomWidth: 1,
+        borderBottomColor: colors.border,
+        alignItems: 'center'
+    },
+    name: {
+        fontFamily: fonts.main,
+        fontSize: fonts.contactNameSize,
+        color: colors.text
+    },
+    avatar: {
+        width: 45,
+        height: 45,
+        borderRadius: 25,
+        margin: 10,
+    }
+})
