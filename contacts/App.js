3f792f427f9d5329e949025452d4fb0fb83134d1
Gabriel Z. Salvi <gabriel.z.salvi@gmail.com>
2022-07-22 09:01:29 -0300

    init contacts project

---
+++ contacts/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+const App = () => {
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <Text>Hello World!</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
