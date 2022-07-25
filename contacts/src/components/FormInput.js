c0d547da225ffb0772e0ec4c72d4c4c9a694eec1
Gabriel Z. Salvi <gabriel.z.salvi@gmail.com>
2022-07-25 10:42:59 -0300

    refactor: create component FormInput

---
+++ contacts/src/components/FormInput.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+
+import {
+    StyleSheet,
+    TextInput,
+    View
+} from 'react-native';
+
+import Icon from 'react-native-vector-icons/Feather';
+import { colors, fonts } from '../../public/styles';
+
+export default props => {
+    const [firstName, setFirstName] = useState()
+
+    const icon = props.icon 
+                    ? <Icon name={props.icon} size={30} color={colors.formIcon} style={styles.icon} />
+                    : null
+                    
+    const inputStyle = icon ? null : { marginLeft: 60 }
+
+    return (
+        <View style={styles.container}>
+            {icon}
+            <TextInput
+                style={[styles.input, inputStyle]}
+                placeholder={props.placeholder}
+                value={firstName}
+                onChangeText={firstName => setFirstName(firstName)}
+            />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+        alignItems: 'center',
+    },
+    input: {
+        width: '70%',
+        fontFamily: fonts.main,
+        fontSize: fonts.inputSize,
+        borderBottomWidth: 1,
+        borderColor: colors.border,
+        padding: 10
+    }
+})
--- contacts/src/screens/AddContact.js
+++ contacts/src/screens/AddContact.js
@@ -1,77 +1,27 @@
 import React, { Component } from 'react';
 import { 
-    FlatList,
-    SafeAreaView,
     StyleSheet,
     Text,
-    TextInput,
     TouchableHighlight,
     View
 } from 'react-native';
 
-import Icon from 'react-native-vector-icons/Feather';
+import FormInput from '../components/FormInput';
 import { colors, fonts } from '../../public/styles';
-import Header from '../components/Header';
-
-const initialState = {
-    firstName: '',
-    lastName: '',
-    phone: '',
-    email: '',
-}
 
 export default class AddContact extends Component {
     constructor(props) {
         super(props);
-        this.state = { ...initialState };
     }
 
     render() {
         return (
-            <>
-            <Header title='Adicionar Contato' />
             <View style={styles.container}>
-                {/* <Header title='Adicionar Contato' /> */}
                 <View style={styles.form}>
-                    <View style={styles.section}>
-                        <Icon name='user' size={30} color={colors.formIcon} style={styles.icon} />
-                        <View style={styles.inputsSection}>
-                            <TextInput
-                                style={styles.input}
-                                placeholder='Nome'
-                                value={this.state.firstName}
-                                onChangeText={firstName => this.setState({ firstName })}
-                            />
-                            <TextInput 
-                                style={styles.input}
-                                placeholder='Sobrenome'
-                                value={this.state.lastName}
-                                onChangeText={lastName => this.setState({ lastName })}
-                            />
-                        </View>
-                    </View>
-                    <View style={styles.section}>
-                        <Icon name='phone' size={30} color={colors.formIcon} style={styles.icon} />
-                        <View style={styles.inputsSection}>
-                            <TextInput
-                                style={styles.input}
-                                placeholder='Nº Telefone'
-                                value={this.state.phone}
-                                onChangeText={phone => this.setState({ phone })}
-                            />
-                        </View>
-                    </View>
-                    <View style={styles.section}>
-                        <Icon name='mail' size={30} color={colors.formIcon} style={styles.icon} />
-                        <View style={styles.inputsSection}>
-                            <TextInput
-                                style={styles.input}
-                                placeholder='Email'
-                                value={this.state.email}
-                                onChangeText={email => this.setState({ email })}
-                            />
-                        </View>
-                    </View>
+                    <FormInput icon='user' placeholder='Nome'/>
+                    <FormInput placeholder='Sobrenome'/>
+                    <FormInput icon='phone' placeholder='Nº Telefone'/>
+                    <FormInput icon='mail' placeholder='Email'/>
                     <View style={styles.buttonsContainer}>
                         <TouchableHighlight style={styles.button}>
                             <Text style={styles.buttonText}>Salvar</Text>
@@ -79,7 +29,6 @@ export default class AddContact extends Component {
                     </View>
                 </View>
             </View>
-            </>
         )
     }
 }
@@ -89,29 +38,9 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center'
-        
     },
     form: {
         width: '90%',
-    },
-    inputsSection: {
-        width: '75%',
-    },
-    input: {
-        width: '100%',
-        fontFamily: fonts.main,
-        fontSize: fonts.inputSize,
-        borderBottomWidth: 1,
-        borderColor: colors.border,
-        padding: 10
-    },
-    section: {
-        flexDirection: 'row',
-        marginHorizontal: 20
-    },
-    icon: {
-        margin: 10,
-        marginRight: 30
     },
     buttonsContainer: {
         marginVertical: 30, 
