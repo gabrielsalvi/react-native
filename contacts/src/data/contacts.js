f6d4f19ee93adb39c788ff87d5818916bbc6f14b
Gabriel Z. Salvi <gabriel.z.salvi@gmail.com>
2022-08-01 16:46:08 -0300

    chore: create file with static data to fill the contacts list

---
+++ contacts/src/data/contacts.js
@@ -0,0 +1,30 @@
+export default [
+    {
+        id: Math.random(),
+        firstName: 'Julio',
+        lastName: 'Cesar',
+        phone: '(54)98414-5616',
+        email: 'julio@gmail.com'
+    },
+    {
+        id: Math.random(),
+        firstName: 'Marechal',
+        lastName: 'Bormann',
+        phone: '49996817331',
+        email: 'marbor@outlook.com'
+    },
+    {
+        id: Math.random(),
+        firstName: 'Thomas',
+        lastName: 'Muller',
+        phone: '54981931243',
+        email: 'muller@outlook.com'
+    },
+    {
+        id: Math.random(),
+        firstName: 'Wandinho',
+        lastName: '',
+        phone: '5499612531833',
+        email: 'wande@gmail.com'
+    },
+]
